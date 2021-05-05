fa9e7be3b25bc0e54cb71787b2feacaea578e5cc
Paul Franke <paul.franke@tu-braunschweig.de>
2021-05-05 02:21:23 -0700

    implement detex function

--- latexbuddy/tools.py
+++ latexbuddy/tools.py
@@ -1,9 +1,8 @@
 import subprocess
 
 
-# example usage in aspell.py at line
+# example usage in aspell.py
 def execute(*cmd):
-
     command = ""
     for arg in cmd:
         command += arg + " "
@@ -13,7 +12,7 @@ def execute(*cmd):
         [command], shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT)
-    out , err_out = error_list.communicate()
+    out, err_out = error_list.communicate()
     return out.decode("ISO8859-1")
 
 
@@ -21,4 +20,3 @@ def detex(file_to_detex):
     detexed_file = file_to_detex + ".detexed"
     execute("detex", file_to_detex, " > ", detexed_file)
     return detexed_file
-
