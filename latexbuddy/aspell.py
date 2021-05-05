deabe167bea182d999dc80d75737768b0b134c87
Paul Franke <paul.franke@tu-braunschweig.de>
2021-05-05 03:11:35 -0700

    implement error class parsing

--- latexbuddy/aspell.py
+++ latexbuddy/aspell.py
@@ -1,40 +1,54 @@
 import shlex
-import subprocess
 import tools
+import error_class
 
-file_to_check = "/home/paul/Desktop/TestProject/main.tex"
-language = "de"
-language = shlex.quote(language)
 
-#check which dictionaries are installed
-"""langs = subprocess.Popen(["aspell dump dicts"], shell=True,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT)"""
+def run(buddy, file):
+    language = "de"
+    language = shlex.quote(language)
+    langs = tools.execute("aspell", "dump dicts")
 
-langs = tools.execute("aspell", "dump dicts", False)
+    # error if language dict not installed
+    if language not in langs:
+        print(
+            "Dict for language \"" + language + "\" not found - [Linux]Install via sudo apt install - check available dicts at https://ftp.gnu.org/gnu/aspell/dict/0index.html")
+        raise Exception("Spell check Failed")
 
-#error if language dict not installed
-if language not in langs:
-    print("Dict for language \"" + language + "\" not found - [Linux]Install via sudo apt install - check available dicts at https://ftp.gnu.org/gnu/aspell/dict/0index.html")
-    raise Exception("Spell check Failed")
+    # execute aspell on given file and collect output
+    error_list = tools.execute("aspell -a --lang=" + language + " < " + file)
 
-#detex file and call aspell on detex output
-error_list = tools.execute("aspell -a --lang=" + language + " < " + file_to_check)
+    # format aspell output
+    out = error_list[70:]
+    out = out.split("\n")
 
-#fetch aspell output
-out = error_list.communicate()
-out = out.decode("ISO8859-1")[70:]
-out = out.split("\n")
+    # cleanup error list
+    cleaned_errors = []
+    for error in out:
+        if error == "":
+            error = "x"
+        if error[0] == '&':
+            cleaned_errors.append(error.replace("&", "").replace("\n", "").strip())
+        if error[0] == '#':
+            cleaned_errors.append(error.replace("#", "").replace("\n", "").strip())
 
-for e in out:
-    print(e)
+    for error in cleaned_errors:
+        print(error)
+        split = error.split(":")
 
-#cleanup error list
-cleaned_errors = []
-for error in out:
-    if error == "":
-        error = "x"
-    if error[0] == '&':
-        cleaned_errors.append(error.replace("&", "").replace("\n", "").strip())
-    if error[0] == '#':
-        cleaned_errors.append(error.replace("#", "").replace("\n", "").strip())
+        temp = split[0].split(" ")
+        location = temp[2] if len(temp) > 2 else temp[1]
+        text = temp[0]
+        sugg = split[1].strip() if len(split) > 1 else ""
+
+        error_class.Error(
+            buddy,
+            file,
+            "aspell",
+            "spelling",
+            "0",
+            text,
+            location,
+            len(text),
+            sugg,
+            False,
+        )
