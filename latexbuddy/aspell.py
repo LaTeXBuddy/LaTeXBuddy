cf76b97c515ffa38a91f255d378af6c06bdc0d60
Paul Franke <paul.franke@tu-braunschweig.de>
2021-05-05 02:38:29 -0700

    change to tool usage

--- latexbuddy/aspell.py
+++ latexbuddy/aspell.py
@@ -1,18 +1,17 @@
 import shlex
 import subprocess
+import tools
 
 file_to_check = "/home/paul/Desktop/TestProject/main.tex"
 language = "de"
 language = shlex.quote(language)
 
 #check which dictionaries are installed
-langs = subprocess.Popen(["aspell dump dicts"], shell=True,
+"""langs = subprocess.Popen(["aspell dump dicts"], shell=True,
                             stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT)
+                            stderr=subprocess.STDOUT)"""
 
-#langs = tools.execute("aspell", "dump dicts", False)
-
-print(langs)
+langs = tools.execute("aspell", "dump dicts", False)
 
 #error if language dict not installed
 if language not in langs:
@@ -20,12 +19,10 @@ if language not in langs:
     raise Exception("Spell check Failed")
 
 #detex file and call aspell on detex output
-error_list = subprocess.Popen(["aspell -a" + " --lang=" + language + " < " + file_to_check], shell=True,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT)
+error_list = tools.execute("aspell -a --lang=" + language + " < " + file_to_check)
 
 #fetch aspell output
-out, err_out = error_list.communicate()
+out = error_list.communicate()
 out = out.decode("ISO8859-1")[70:]
 out = out.split("\n")
 
