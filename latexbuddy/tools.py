2bffab5b0b89f6e51a799c5243da44b544243664
Paul Nowitzki <p.nowitzki@tu-braunschweig.de>
2021-05-06 13:52:09 +0200

    added list and background options for execute(), added type declarations

--- latexbuddy/tools.py
+++ latexbuddy/tools.py
@@ -2,11 +2,8 @@ import subprocess
 
 
 # example usage in aspell.py
-def execute(*cmd):
-    command = ""
-    for arg in cmd:
-        command += arg + " "
-    command.strip()
+def execute(*cmd: str) -> str:
+    command = get_command_string(cmd)
 
     error_list = subprocess.Popen(
         [command], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
@@ -15,7 +12,37 @@ def execute(*cmd):
     return out.decode("ISO8859-1")
 
 
+def execute_from_list(cmd: list[str]) -> str:
+    return execute(*cmd)
+
+
+def execute_background(*cmd: str) -> subprocess.Popen:
+    command = get_command_string(cmd)
+
+    process = subprocess.Popen(
+        [command], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+    )
+    return process
+
+
+def execute_background_from_list(cmd: list[str]) -> subprocess.Popen:
+    return execute_background(*cmd)
+
+
+def get_command_string(*cmd: str) -> str:
+    command = ""
+    for arg in cmd:
+        command += arg + " "
+    return command.strip()
+
+
 def detex(file_to_detex):
     detexed_file = file_to_detex + ".detexed"
     execute("detex", file_to_detex, " > ", detexed_file)
     return detexed_file
+
+
+if __name__ == "__main__":
+    command_ = ["echo", "hi,", "what\\'s", "up?"]
+    print(execute(*command_))
+    # print(execute_from_list(command_))
