62ae1378e3cb0c0be5d504eb616793d99b60645b
Niklas Pieninck <n.pieninck@tu-braunschweig.de>
2021-05-10 10:22:03 +0200

    Change misuse of dictionary.

--- latexbuddy/error_class.py
+++ latexbuddy/error_class.py
@@ -19,10 +19,18 @@ class Error:
         suggestions,
         warning,
     ):
-        self.dict = {"path": path, "src": src, "error_type": error_type,
-                      "error_id": error_id,
-                      "text": text, "start": start, "length": length,
-                      "suggestions": suggestions, "warning": warning, "uid": self.uid()}
+        self.dict = {
+            "path": path,
+            "src": src,
+            "error_type": error_type,
+            "error_id": error_id,
+            "text": text,
+            "start": start,
+            "length": length,
+            "suggestions": suggestions,
+            "warning": warning,
+            "uid": self.uid(),
+        }
         buddy.add_error(self)
 
     """
@@ -31,8 +39,12 @@ class Error:
 
     def uid(self):
         return "{}\0{}\0{}\0{}\0{}\0{}".format(
-            self.error['path'], self.['src'], self.['error_type'], self.['error_id'],
-            self.['start'], self.['length']
+            self.dict["path"],
+            self.dict["src"],
+            self.dict["error_type"],
+            self.dict["error_id"],
+            self.dict["start"],
+            self.dict["length"],
         )
 
     """
@@ -40,13 +52,17 @@ class Error:
     """
 
     def get_uid(self):
-        return self.uid
+        return self.dict["uid"]
 
     # TODO: maybe different hashes for different error types
     def get_hash(self, language):
-        string_for_hash = self.error_type + self.text + language
+        string_for_hash = self.dict["error_type"] + self.dict["text"] + language
         return hashlib.md5(string_for_hash).hexdigest()
 
     def __eq__(self, other):
-        return self.error_type == other.error_type & self.text == other.text  # &\
+        return (
+            self.dict["error_type"]
+            == other.dict["error_type"] & self.dict["text"]
+            == other.dict["text"]
+        )  # &\
         # .error_id == other.error_id & self.src == other.src
