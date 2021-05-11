558f728dae94b86c9f60e9d72847878513161102
Paul Nowitzki <p.nowitzki@tu-braunschweig.de>
2021-05-11 23:54:09 +0200

    added example config.py with some test values

---
+++ latexbuddy/config.py
@@ -0,0 +1,14 @@
+"""
+This is an example configuration for LaTeXBuddy and the tools that are supported
+out-of-the-box. You may move this file and specify its path using the --config flag.
+"""
+
+modules = {
+    "latexbuddy": {
+        "key": "value",
+        "answer to life": 42
+    },
+    "languagetool": {
+        "mode": "LOCAL_SERVER",
+    },
+}
