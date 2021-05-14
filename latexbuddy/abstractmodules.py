8636c4dc5ec07ce3e4482e7ed9b32c746b749864
Lennart Lutz <l.lutz@tu-braunschweig.de>
2021-05-14 18:58:07 +0000

    black correction abstractmodules.py

--- latexbuddy/abstractmodules.py
+++ latexbuddy/abstractmodules.py
@@ -2,25 +2,24 @@ from abc import ABC, abstractmethod
 
 
 class Modules(ABC):
-
     @abstractmethod
     def run(self):
         pass
 
 
 class Aspell(Modules):
-
     # overriding abstract method to run aspell
     def run(self, buddy, file):
         import latexbuddy.aspell as aspell
+        
         aspell.run(buddy, file)
 
 
 class Languagetool(Modules):
-
     # overriding abstract method
     def run(self, buddy, file):
         import latexbuddy.languagetool as languagetool
+
         languagetool.run(buddy, file)
 
 
@@ -29,4 +28,5 @@ class Chktex(Modules):
     # overriding abstract method
     def run(self, buddy, file):
         import latexbuddy.chktex as chktex
+
         chktex.run(buddy, file)
