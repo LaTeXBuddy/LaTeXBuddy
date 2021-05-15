f7e4fc2e180ce4799f5b3a267e39d0781a743c5e
Maik Weber <maik.weber@tu-braunschweig.de>
2021-05-15 14:51:56 +0200

    Formatting and type safety

--- latexbuddy/cli.py
+++ latexbuddy/cli.py
@@ -1,7 +1,6 @@
 import argparse
-import pathlib
 
-from typing import Optional
+from pathlib import Path
 
 from latexbuddy.config_loader import ConfigLoader
 from latexbuddy.latexbuddy import LatexBuddy
@@ -9,33 +8,37 @@ from latexbuddy.latexbuddy import LatexBuddy
 
 parser = argparse.ArgumentParser(description="The one-stop-shop for LaTeX checking.")
 
-parser.add_argument("file", help="File that will be processed.")
+parser.add_argument(
+    "file",
+    type=Path,
+    help="File that will be processed."
+)
 parser.add_argument(
     "--config",
     "-c",
-    type=Optional[pathlib.Path],
-    default=None,
+    type=Path,
+    default=Path("config.py"),
     help="Location of the config file.",
 )
 
 parser.add_argument(
     "--language",
     "-l",
-    type=Optional[str],
+    type=str,
     default=None,
     help="Target language of the file.",
 )
 parser.add_argument(
     "--whitelist",
     "-w",
-    type=Optional[pathlib.Path],
+    type=Path,
     default=None,
     help="Location of the whitelist file.",
 )
 parser.add_argument(
     "--output",
     "-o",
-    type=Optional[pathlib.Path],
+    type=Path,
     default=None,
     help="Where to output the errors file.",
 )
@@ -50,6 +53,7 @@ def main():
         config_loader=config_loader,
         file_to_check=args.file,
     )
+
     buddy.run_tools()
     buddy.check_whitelist()
     buddy.parse_to_json()
