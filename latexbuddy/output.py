1448d59d86a13441ea70138847a1c13086978d33
Niklas Pieninck <n.pieninck@tu-braunschweig.de>
2021-05-15 15:33:44 +0200

    Disable code highlighting

--- latexbuddy/output.py
+++ latexbuddy/output.py
@@ -5,6 +5,7 @@ env = Environment(loader=PackageLoader("latexbuddy"))
 
 
 def render_html(file_name, file_text, errors):
+    err_values = sorted(errors.values(), key=lambda e: int(e.start))
     template = env.get_template("result.html")
     highlighted_file_text = highlight_code(file_text, errors)
     return template.render(
@@ -13,17 +14,17 @@ def render_html(file_name, file_text, errors):
 
 
 def highlight_code(file_text, errors):
-    offset = 0
-    for error in errors.values():
-        start_index = error.start + offset
-        end_index = start_index + error.length
-        file_text = (
-            file_text[:start_index]
-            + "<u>"
-            + file_text[start_index:end_index]
-            + "</u>"
-            + file_text[end_index:]
-        )
-        offset += len("<u></u>")
+    # offset = 0
+    # for error in sorted(errors.values(), key=lambda e: int(e.start)):
+    #     start_index = int(error.start) + offset
+    #     end_index = start_index + int(error.length)
+    #     file_text = (
+    #         file_text[:start_index]
+    #         + "<u>"
+    #         + file_text[start_index:end_index]
+    #         + "</u>"
+    #         + file_text[end_index:]
+    #     )
+    #     offset += len("<u></u>")
 
     return file_text
