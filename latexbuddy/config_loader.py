646b757350433065ecae1495fd864005ca69a747
Maik Weber <maik.weber@tu-braunschweig.de>
2021-05-15 13:10:39 +0200

    Implemented get_config_option()

--- latexbuddy/config_loader.py
+++ latexbuddy/config_loader.py
@@ -5,6 +5,10 @@ import traceback
 from typing import Any
 
 
+class ConfigOptionNotFoundError(Exception):
+    pass
+
+
 class ConfigLoader:
     def __init__(self, config_file_path: str):
 
@@ -43,10 +47,20 @@ class ConfigLoader:
             traceback.print_exc(file=sys.stderr)
 
     def get_config_option(self, tool_name: str, key: str) -> Any:
-        raise NotImplementedError()
+
+        if (
+            tool_name not in self.configurations
+            or key not in self.configurations[tool_name]
+        ):
+            raise ConfigOptionNotFoundError(f"Tool: {tool_name}, key: {key}")
+
+        return self.configurations[tool_name][key]
 
     def get_config_option_or_default(
         self, tool_name: str, key: str, default_value: Any
     ) -> Any:
 
-        raise NotImplementedError()
+        try:
+            return self.get_config_option(tool_name, key)
+        except ConfigOptionNotFoundError:
+            return default_value
