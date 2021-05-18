5a379a2e9810ed6e8575736b8ab34750d66410e9
Paul Nowitzki <p.nowitzki@tu-braunschweig.de>
2021-05-18 20:05:38 +0200

    added type-hints and improved docstrings

--- latexbuddy/abs_module.py
+++ latexbuddy/abs_module.py
@@ -1,18 +1,31 @@
 """This module defines the abstract module of LaTeXBuddy."""
 
 from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import List
+
+from latexbuddy.error_class import Error
+from latexbuddy.latexbuddy import LatexBuddy
 
 
 class Module(ABC):
     """Abstract class that defines a simple LaTeXBuddy module."""
 
     @abstractmethod
-    def run_module(self, buddy, file_path):
-        """Runs the checks for the respective module."""
+    def run_module(self, buddy: LatexBuddy, file_path: Path) -> None:
+        """Runs the checks for the respective module and collects
+            errors internally.
+
+        :param buddy: the calling LaTeXBuddy instance (for config access)
+        :param file_path: path of the file to be checked
+        """
         pass
 
-    # TODO: define attributes for method
     @abstractmethod
-    def save_errors(self):
-        """Saves the Errors into the error_class"""
+    def get_errors(self, buddy: LatexBuddy) -> List[Error]:
+        """Passes all currently accumulated errors to the caller.
+
+        :param buddy: the calling LaTeXBuddy instance (for config access)
+        :return: a list of all acumulated errors in this module
+        """
         pass
