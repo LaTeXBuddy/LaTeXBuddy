78b2682b1193db36db289ec3f2ea20f6ed8e687f
Paul Franke <paul.franke@tu-braunschweig.de>
2021-05-21 07:21:41 -0700

    use regex for easier parsing

--- latexbuddy/modules/diction.py
+++ latexbuddy/modules/diction.py
@@ -7,6 +7,7 @@ from unidecode import unidecode
 
 import latexbuddy.buddy as ltb
 import latexbuddy.tools as tools
+import re
 
 from latexbuddy.abs_module import Module
 from latexbuddy.problem import Problem, ProblemSeverity
@@ -23,6 +24,9 @@ class DictionModule(Module):
 
         # replace umlauts so error position is correct
         lines = Path(file.plain_file).read_text()
+        original_lines = lines.splitlines(keepends=True)
+
+        #print(original_lines)
         lines = unidecode(lines)
 
         # write cleaned text to tempfile
@@ -48,32 +52,56 @@ class DictionModule(Module):
         # remove temp file
         os.remove(cleaned_file)
 
-        return self.format_errors(cleaned_errors, file.plain_file)
+        # return list of Problems
+        return self.format_errors(cleaned_errors, original_lines, file.plain_file)
 
-    def format_errors(self, out: List[str], file) -> List[Problem]:
+    def format_errors(self, out: List[str], original: List[str], file) -> List[Problem]:
         """Parses diction errors and returns list of Problems.
 
+        :param original: lines of file to check as list
         :param out: line stripped output of the diction command with empty lines removed
         :param file: the file path
         """
         problems = []
         for error in out:
 
+            print(error)
+
+            o_line = ""
             if "Double word" in error:
+                pattern = re.escape("[") + ".*" + re.escape("->") + ".*" + re.escape("]")
+                matches = re.findall(pattern, error)
+                print(matches)
+
+                # TODO: will be replaced by regex soon
                 src, lines, chars, sugg = error.split(":", maxsplit=3)
+                sugg = matches;
                 splitted_lines = lines.split("-")
                 splitted_chars = chars.split("-")
                 splitted_lines_int = [int(a) for a in splitted_lines]
                 splitted_chars_int = [int(a) for a in splitted_chars]
                 start_line, end_line = splitted_lines_int[0], splitted_lines_int[1]
-                start_char, end_char = splitted_chars_int[0], splitted_chars_int[1]
-                # print(
-                #    f"Double word error: src={src}, start_line={start_line},end_line={end_line},start_char={start_char}, end_char={end_char}, sugg={sugg}"
-                # )
+                start_char, end_char = splitted_chars_int[0] - 1, splitted_chars_int[1]
+                if start_line == end_line:
+                    o_line = original[start_line-1][start_char-1:end_char]
+                else:
+                    for x in range(start_line, end_line + 1):
+                        if x == end_line:
+                            o_line = o_line + original[x - 1][:end_char]
+                        elif x == start_line:
+                            o_line = o_line + original[x - 1][start_char:]
+                        else:
+                            o_line = o_line + original[x - 1]
+                #print(
+                  #  f"Double word error: src={src}, start_line={start_line},end_line={end_line},start_char={start_char}, end_char={end_char}, sugg={sugg}, o_line={o_line}"
+                #)
                 location = (start_line, start_char)
+
+
             else:
+                # TODO: will be replaced by regex soon
                 src, location, sugg = error.split(":", maxsplit=2)
-                # print(f"Wording error: src={src}, loc={location}, sugg={sugg}")
+                #print(f"Wording error: src={src}, loc={location}, sugg={sugg}")
                 line = int(location.split(".")[0])
                 num = int(location.split(".")[1].split("-")[0])
                 location = (line, num)
@@ -81,7 +109,7 @@ class DictionModule(Module):
             problems.append(
                 Problem(
                     position=location,
-                    text=sugg,
+                    text=o_line,
                     checker="diction",
                     cid="0",
                     file=file,
