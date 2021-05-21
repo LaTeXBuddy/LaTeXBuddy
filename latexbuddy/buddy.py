4d32628b5fe079f0632f3a27818a124ad66fab1d
Niklas Pieninck <n.pieninck@tu-braunschweig.de>
2021-05-21 08:14:13 +0200

    Test out marking text.

--- latexbuddy/buddy.py
+++ latexbuddy/buddy.py
@@ -148,31 +148,31 @@ class LatexBuddy:
         detexed_file, self.charmap, detex_err = tools.yalafi_detex(self.file_to_check)
         self.check_successful = True if len(detex_err) < 1 else False
 
-        for err in detex_err:
-            self.add_error(
-                # TODO: Verify this is correct and maybe implement more attributes
-                Problem(
-                    err[0],
-                    err[1],
-                    "YALaFi",
-                    "latex",
-                    self.file_to_check,
-                )
-                # TODO: old implementation for reference (remove when finished)
-                # Problem(
-                #    self,
-                #    str(self.file_to_check),
-                #    "YALaFi",
-                #    "latex",
-                #    "TODO",
-                #    err[1],
-                #    err[0],
-                #    0,
-                #    [],
-                #    False,
-                #    "TODO",
-                # )
-            )
+        # for err in detex_err:
+        #     self.add_error(
+        #         # TODO: Verify this is correct and maybe implement more attributes
+        #         Problem(
+        #             err[0],
+        #             err[1],
+        #             "YALaFi",
+        #             "latex",
+        #             self.file_to_check,
+        #         )
+        #         # TODO: old implementation for reference (remove when finished)
+        #         # Problem(
+        #         #    self,
+        #         #    str(self.file_to_check),
+        #         #    "YALaFi",
+        #         #    "latex",
+        #         #    "TODO",
+        #         #    err[1],
+        #         #    err[0],
+        #         #    0,
+        #         #    [],
+        #         #    False,
+        #         #    "TODO",
+        #         # )
+        #     )
 
         tool_loader = ToolLoader(Path("latexbuddy/modules/"))
         modules = tool_loader.load_selected_modules(self.cfg)
@@ -251,23 +251,75 @@ class LatexBuddy:
                 end = start + max(i)
                 opening_tag = f'<mark>'
                 closing_tag = f'</mark>'
-                string = tex_lines[line][:start] + opening_tag + tex_lines[line][start:end] + closing_tag + tex_lines[line]
+                string = tex_lines[line][:start] + opening_tag + tex_lines[line][
+                                                                 start:end] + closing_tag + \
+                         tex_lines[line]
                 new_len = len(string)
                 tex_lines[line] = string
                 offset += new_len - old_len
 
         return "".join(tex_lines)
 
-
-
     def output_html(self):
 
         # importing this here to avoid circular import error
         from latexbuddy.output import render_html
 
-        # err_values = sorted(self.errors.values(), key=output.error_key)
-        # html_output_path = Path(self.error_file + ".html")
+        err_values = sorted(sorted(self.errors.values(), key=output.error_key),
+                            key=lambda e: e.position[1])
+        html_output_path = Path(self.error_file + ".html")
         # html = self.iwas(err_values, self.file_to_check.read_text())
-        # html_output_path.write_text(html)
+        html = self.mark_output(self.file_to_check, self.file_to_check.read_text(),
+                                err_values)
+        html_output_path.write_text(html)
 
         print(f"File output to {html_output_path}")
+
+    def mark_output(self, file, text, problems) -> str:
+        html = output.render_html(file, text, self.errors)
+        soup = BeautifulSoup(html, 'html.parser')
+        lines_problems = self.get_line_problems(problems, text)
+        lines = text.split('\n')
+        line_count = len(lines)
+        end_span = unescape('</span>')
+        for line in range(line_count):
+            offset = 0
+            for problem in lines_problems[line]:
+                if problem.checker == 'aspell':
+                    continue
+                start = problem.position[1] + offset - 1
+                end = start + problem.length
+                span = self.get_span(problem)
+                left = lines[line][:start]
+                middle = span + lines[line][start:end] + end_span
+                right = lines[line][end:]
+                lines[line] = left + middle + right
+                offset += len(span) + len(end_span)
+
+        content = ''
+        for line in lines:
+            content += line + '\n'
+
+        new_code = soup.new_tag('code')
+        new_code.string = unescape(content)
+        soup.find('section', id='file-contents').pre.code.replace_with(new_code)
+
+        return unescape(str(soup))
+
+    @staticmethod
+    def get_line_problems(problems, text) -> dict[int, list[Problem]]:
+        problem_dict = {}
+        line_count = len(text.split('\n'))
+        for line in range(line_count):
+            problem_dict[line] = []
+
+        for problem in problems:
+            if problem.checker == 'aspell':
+                continue
+            problem_dict[problem.position[0] - 1].append(problem)
+
+        return problem_dict
+
+    @staticmethod
+    def get_span(problem) -> str:
+        return unescape('<span class="' + problem.category + '">')
