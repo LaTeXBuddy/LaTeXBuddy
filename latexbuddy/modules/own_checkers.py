26579ebe182c8073979d7b62788748927d0dc48c
Niklas Pieninck <n.pieninck@tu-braunschweig.de>
2021-05-27 13:37:51 +0200

    Add positions.

--- latexbuddy/modules/own_checkers.py
+++ latexbuddy/modules/own_checkers.py
@@ -2,6 +2,7 @@ from latexbuddy.buddy import LatexBuddy
 from latexbuddy.modules import Module
 from latexbuddy.problem import Problem, ProblemSeverity
 from latexbuddy.texfile import TexFile
+import latexbuddy.tools as tools
 import re
 
 
@@ -10,6 +11,7 @@ class UnreferencedFiguresModule(Module):
         self.tool_name = 'refcheck'
         self.cid = '0'
         self.severity = ProblemSeverity.INFO
+        self.category = 'latex'
 
     def run_checks(self, buddy: LatexBuddy, file: TexFile) -> list[Problem]:
         tex = file.tex
@@ -18,26 +20,36 @@ class UnreferencedFiguresModule(Module):
         pattern = r'\\begin{figure}[\w\W]*?\\end{figure}'
         figures = re.findall(pattern, tex)
         len_label = len('label{')
-        labels = []
+        labels = {}
         for figure in figures:
+            match = re.search(re.escape(figure), tex)
+            absolute_position = match.span()[0]
+            length = match.span()[1] - match.span()[0]
             split = figure.split('\\')
             for word in split:
                 if re.search(re.escape('label{') + '.*' + re.escape('}'),
                              word) is not None:
                     label = word[len_label:len(word) - 2]
-                    labels.append(label)
-        for label in labels:
+                    labels[label] = (absolute_position, length)
+        for label in labels.keys():
+            pos_len = labels[label]
+            position = pos_len[0]
+            length = pos_len[1]
+            line, col, offset = tools.absolute_to_linecol(file.tex, position)
             if re.search(re.escape('\\ref{') + label + re.escape('}'), tex) is None:
                 problems.append(
                     Problem(
-                        position=(0, 0),
+                        position=(line, col),
                         text=label,
                         checker=self.tool_name,
+                        category=self.category,
                         cid=self.cid,
                         file=file.tex_file,
                         severity=self.severity,
                         description=f'Figure {label} not referenced.',
                         key=self.tool_name + '_' + label,
+                        length=length,
+                        context=('\\label{', '}')
                     )
                 )
 
