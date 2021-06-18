dd76e9a77dc56b42858f0e7df0704d980ccf4ee1
Maik Weber <maik.weber@tu-braunschweig.de>
2021-06-18 12:49:34 +0200

    Fixed UnreferencedFiguresModule regex usage to find multiple occurrences of same string

--- latexbuddy/modules/own_checkers.py
+++ latexbuddy/modules/own_checkers.py
@@ -27,25 +27,21 @@ class UnreferencedFiguresModule(Module):
         tex = file.tex
         problems = []
         pattern = r"\\begin{figure}[\w\W]*?\\end{figure}"
-        figures = re.findall(pattern, tex)
+        figures = re.finditer(pattern, tex)
         len_label = len("label{")
         labels = {}
-        for figure in figures:
-            match = re.search(re.escape(figure), tex)
-            absolute_position = match.span()[0]
-            length = match.span()[1] - match.span()[0]
-            split = figure.split("\\")
+
+        for figure_match in figures:
+            start, end = figure_match.span()
+            length = end - start
+            split = figure_match.group(0).split("\\")
             for word in split:
-                if (
-                    re.search(re.escape("label{") + ".*" + re.escape("}"), word)
-                    is not None
-                ):
-                    label = word[len_label : len(word) - 2]
-                    labels[label] = (absolute_position, length)
-        for label in labels.keys():
-            pos_len = labels[label]
-            position = pos_len[0]
-            length = pos_len[1]
+                label_match = re.search(re.escape("label{") + "(.*)" + re.escape("}"), word)
+                if label_match is not None:
+                    label = label_match.group(1)
+                    labels[(start, length)] = label
+
+        for (position, length), label in labels.items():
             line, col, offset = tools.absolute_to_linecol(file.tex, position)
             if re.search(re.escape("\\ref{") + label + re.escape("}"), tex) is None:
                 problems.append(
