05017fc4a19d8029036f0c5b4eda6fb01fcf9d34
Paul Nowitzki <p.nowitzki@tu-braunschweig.de>
2021-06-26 01:12:13 +0200

    updated format to match the new config format

--- config.py
+++ config.py
@@ -3,15 +3,16 @@ This is an example configuration for LaTeXBuddy and the tools that are supported
 out-of-the-box. You may move this file and specify its path using the --config flag.
 """
 
+main = {
+    "language": "en",
+    "language_country": "GB",
+    "whitelist": "whitelist",
+    "output": "./",
+    "format": "HTML",
+    "enable-modules-by-default": True,
+}
+
 modules = {
-    "LatexBuddy": {
-        "language": "en",
-        "language_country": "GB",
-        "whitelist": "whitelist",
-        "output": "./",
-        "format": "HTML",
-        "enable-modules-by-default": True,
-    },
     "AspellModule": {
         "enabled": True,
     },
