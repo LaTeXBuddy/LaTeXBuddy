a5a1f23ddc1f2cf657eb6ea00821310f432e626f
Paul Nowitzki <p.nowitzki@tu-braunschweig.de>
2021-07-04 22:57:23 +0200

    added resources for testcase T700

---
+++ tests/pytest_testcases/integration_tests/resources/T700_config.py
@@ -0,0 +1,23 @@
+"""
+This is an example configuration for LaTeXBuddy and the tools that are supported
+out-of-the-box. You may move this file and specify its path using the --config flag.
+"""
+
+main = {
+    "language": "en",
+    "language_country": "GB",
+    "module_dir": "tests/pytest_testcases/integration_tests/resources/T700_driver_modules/",
+    "output": "./latexbuddy_html/",
+    "format": "HTML",
+    "enable-modules-by-default": False,
+    "pdf": True,
+}
+
+modules = {
+    "FirstDriver": {
+        "enabled": True,
+    },
+    "SecondDriver": {
+        "enabled": True,
+    },
+}
