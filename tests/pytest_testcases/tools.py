c9ee48b0dd5268df5b11ad116a7fd25544b11ca9
Paul Nowitzki <p.nowitzki@tu-braunschweig.de>
2021-07-04 22:55:33 +0200

    added tools for testing

---
+++ tests/pytest_testcases/tools.py
@@ -0,0 +1,30 @@
+import subprocess
+
+
+def get_command_string(cmd):
+    """Constructs a command string from a tuple of arguments.
+
+    :param cmd: tuple of command line arguments
+    :return: the command string
+    """
+    command = ""
+    for arg in cmd:
+        command += str(arg) + " "
+    return command.strip()
+
+
+def execute_and_collect(*cmd, encoding="ISO8859-1"):
+    """Executes a terminal command with subprocess.
+
+    :param cmd: command name and arguments
+    :param encoding: output encoding
+    :return: command output
+    """
+
+    command = get_command_string(cmd)
+
+    error_list = subprocess.Popen(
+        [command], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
+    out, err_out = error_list.communicate()
+    return out.decode(encoding)
