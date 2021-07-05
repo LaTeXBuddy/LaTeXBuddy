e532075fa37991488b710f65e1678fecc5b0bf3e
Niklas Pieninck <n.pieninck@tu-braunschweig.de>
2021-07-05 17:13:08 +0200

    T1900 Add new file structure

--- tests/pytest_testcases/unit_tests/test_T1900_logfilter.py
+++ tests/pytest_testcases/unit_tests/test_T1900_logfilter.py
@@ -2,7 +2,11 @@ import pytest
 import os
 
 from pathlib import Path
+from tempfile import mkstemp
 from tests.pytest_testcases.tools import execute_and_collect
+from latexbuddy.modules.logfilter import LogFilter
+from latexbuddy.texfile import TexFile
+from resources.dummy_config_loader import ConfigLoader
 
 
 @pytest.fixture
@@ -10,18 +14,44 @@ def script_dir():
     return str(Path(os.path.realpath(__file__)).parents[0])
 
 
-def test_run_checks(script_dir):
-    config_path = script_dir + "/resources/T1900_config.py"
+def test_all_problems_in_texfilt(script_dir):
+    # config_path = script_dir + "/resources/T1900_config.py"
     document_path = script_dir + "/resources/T1900.tex"
+    texfilt_path = script_dir + "/latexbuddy/modules/texfilt.awk"
+    log_filter = LogFilter()
+    config_loader = ConfigLoader()
+    tex_file = TexFile(document_path)
+    descriptor, raw_problems_path = mkstemp(
+        prefix="latexbuddy", suffix="raw_log_errors"
+    )
 
-    output_path = script_dir + "../../../latexbuddy_html/latexbuddy_output.json"
+    problems = log_filter.run_checks(config_loader, tex_file)
 
-    buddy_run = [
-        "latexbuddy",
-        "--config", config_path,
-        "--format", "JSON",
-        document_path,
+    cmd = [
+        "awk", "-f",
+        texfilt_path,
+        f"{str(tex_file.log_file)} > {raw_problems_path}",
     ]
-    run_output = execute_and_collect(*buddy_run)
-    Path('output_T1900.test').write_text(run_output)
-    assert len(run_output) > 0
+    result = execute_and_collect(*cmd)  # not used
+
+
+def test_all_texfilt_in_problems(script_dir):
+    # config_path = script_dir + "/resources/T1900_config.py"
+    document_path = script_dir + "/resources/T1900.tex"
+    texfilt_path = script_dir + "/latexbuddy/modules/texfilt.awk"
+    log_filter = LogFilter()
+    config_loader = ConfigLoader()
+    tex_file = TexFile(document_path)
+    descriptor, raw_problems_path = mkstemp(
+        prefix="latexbuddy", suffix="raw_log_errors"
+    )
+
+    problems = log_filter.run_checks(config_loader, tex_file)
+
+    cmd = [
+        "awk", "-f",
+        texfilt_path,
+        f"{str(tex_file.log_file)} > {raw_problems_path}",
+    ]
+    result = execute_and_collect(*cmd)  # not used
+
