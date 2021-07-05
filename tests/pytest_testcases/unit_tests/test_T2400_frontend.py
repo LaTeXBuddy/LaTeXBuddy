ec136dee342bc06e363fd92fe2cc893413d6d413
Lennart Lutz <l.lutz@tu-braunschweig.de>
2021-07-05 05:58:10 -0700

    T2400 added

---
+++ tests/pytest_testcases/unit_tests/test_T2400_frontend.py
@@ -0,0 +1,11 @@
+from latexbuddy.output import render_html
+
+
+def test_render_html():
+    file_name = "hello"
+    file_text = "Test.!"
+    problemlist = []  # TODO: ass some problems
+    path_list = []  # TODO: add some paths
+    pdf_path = "lel"  # TODO: add a path to a real pdf file
+
+    render_html()
