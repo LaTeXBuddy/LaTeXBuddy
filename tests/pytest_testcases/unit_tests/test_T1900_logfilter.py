ced31297586124f2270d26d8d4e8b16684346c07
Niklas Pieninck <n.pieninck@tu-braunschweig.de>
2021-07-05 08:30:44 +0200

    Update T1900

--- tests/pytest_testcases/unit_tests/test_T1900_logfilter.py
+++ tests/pytest_testcases/unit_tests/test_T1900_logfilter.py
@@ -1,6 +1,12 @@
 import pytest
+import os
+
+from pathlib import Path
 from tests.pytest_testcases.tools import execute_and_collect
 
+@pytest.fixture
+def script_dir():
+    return str(Path(os.path.realpath(__file__)).parents[0])
 
 def test_run_checks(script_dir):
     config_path = script_dir + "/resources/T1900_config.py"
@@ -8,12 +14,18 @@ def test_run_checks(script_dir):
 
     output_path = script_dir + "../../../latexbuddy_html"
 
-    cmd = [
+    buddy_run = [
         "latexbuddy",
-        "--verbose",
         "--config", config_path,
         "--format", "JSON",
         "--output", output_path,
         document_path,
     ]
-    result = execute_and_collect(*cmd)
+    run_output = execute_and_collect(*buddy_run)    # not used
+    assert len(run_output) > 0
+    output_json = [
+        'cat',
+        output_path + '/T1900_test_document.json'
+    ]
+    json_output = execute_and_collect(*output_json)
+    assert len(json_output) > 0
