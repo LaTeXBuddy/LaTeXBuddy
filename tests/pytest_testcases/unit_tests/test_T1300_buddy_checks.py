5da9b88184a8deeab55017612fb1224222b98443
Paul Nowitzki <p.nowitzki@tu-braunschweig.de>
2021-07-11 21:44:31 +0200

    fixed format

--- tests/pytest_testcases/unit_tests/test_T1300_buddy_checks.py
+++ tests/pytest_testcases/unit_tests/test_T1300_buddy_checks.py
@@ -10,8 +10,8 @@ from latexbuddy.buddy import LatexBuddy
 from latexbuddy.config_loader import ConfigLoader
 from latexbuddy.module_loader import ModuleProvider
 from latexbuddy.modules import Module
-from tests.pytest_testcases.unit_tests.resources.T1300_dummy_modules.dummy_module import \
-    DummyModule0, DummyModule1
+from tests.pytest_testcases.unit_tests.resources.T1300_dummy_modules.dummy_module \
+    import DummyModule0, DummyModule1
 
 
 @pytest.fixture
