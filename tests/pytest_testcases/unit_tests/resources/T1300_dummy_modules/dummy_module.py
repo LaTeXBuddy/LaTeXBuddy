dd89ac1b0c9b0e411d3c5d3fda2df00d6925849d
Paul Nowitzki <p.nowitzki@tu-braunschweig.de>
2021-07-11 21:10:23 +0200

    added dummy modules for testcase T1300 (buddy-checks)

---
+++ tests/pytest_testcases/unit_tests/resources/T1300_dummy_modules/dummy_module.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from latexbuddy.config_loader import ConfigLoader
+from latexbuddy.modules import Module
+from latexbuddy.problem import Problem, ProblemSeverity
+from latexbuddy.texfile import TexFile
+
+
+class DummyModule0(Module):
+    def __init__(self):
+        pass
+
+    def run_checks(self, config: ConfigLoader, file: TexFile) -> List[Problem]:
+        return [
+            Problem(
+                (1, 1),
+                "text",
+                DummyModule0,
+                file.tex_file,
+                ProblemSeverity.ERROR,
+                "my_problem_id",
+                category="dumb mistakes",
+                description="this is a test error, please ignore it...",
+                context=("", "totally made up"),
+                suggestions=["idk", "try harder next time"],
+                key="my_very_unique_key",
+            )
+        ]
+
+
+class DummyModule1(Module):
+    def __init__(self):
+        pass
+
+    def run_checks(self, config: ConfigLoader, file: TexFile) -> List[Problem]:
+        return [
+            Problem(
+                (1, 2),
+                "text2",
+                DummyModule0,
+                file.tex_file,
+                ProblemSeverity.WARNING,
+            )
+        ]
